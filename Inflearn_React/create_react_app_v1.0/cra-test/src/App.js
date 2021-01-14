72e0a999389d2ed914b23f71f3f4165d4c691898
jysaa5 <jysaa5@naver.com>
2021-01-14 23:13:43 +0900

    Study React

--- Inflearn_React/create_react_app_v1.0/cra-test/src/App.js
+++ Inflearn_React/create_react_app_v1.0/cra-test/src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import pengsoo1 from './pengsoo1.jpg';
+import pengsoo2 from './Pengsoo2.jpg';
 
 function App() {
 
@@ -13,6 +15,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <img src={pengsoo1} className="App-logo" alt="logo"/>
+        <img src={pengsoo2} className="App-logo" alt="logo"/>
         <button onClick={onClick}>데이터를 보여주세요</button>
         <p>
           hello!
