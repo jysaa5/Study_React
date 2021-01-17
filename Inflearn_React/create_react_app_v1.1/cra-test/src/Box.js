f747d7ccaa9b93297e4039c56fd48193600358ae
jysaa5 <jysaa5@naver.com>
2021-01-18 02:00:41 +0900

    Study React

--- Inflearn_React/create_react_app_v1.1/cra-test/src/Box.js
+++ Inflearn_React/create_react_app_v1.1/cra-test/src/Box.js
@@ -1,6 +1,7 @@
 import React from 'react';
 // import './Box.css';
-import Style from './Box.module.css';
+//import Style from './Box.module.css';
+import styled from 'styled-components';
 
 // 요소들을 렌더링하고 있음.
 // export default function Box({size}){
@@ -11,11 +12,30 @@ import Style from './Box.module.css';
 //     }
 // }
 
+/* css-module */
+// export default function Box({size}){
+//     if(size==='big'){
+//         return <div className={`${Style.box} ${Style.big}`}>큰 박스</div>;
+//     }else{
+//         return <div className={`${Style.box} ${Style.small}`}>작은 박스</div>;
+//     }
+// }
+
+/* css-in-js */
+// 컴포넌트 생성
+const BoxCommon = styled.div`
+height: 50px;
+background-color: #aaaaaa;`;
+
+// 위 컴포넌트를 확장
+const BoxBig = styled(BoxCommon)`width: 200px;`;
+
+const BoxSmall = styled(BoxCommon)`width: 100px`;
 
 export default function Box({size}){
-    if(size==='big'){
-        return <div className={`${Style.box} ${Style.big}`}>큰 박스</div>;
+    if(size === 'big'){
+        return <BoxBig>큰 박스</BoxBig>;
     }else{
-        return <div className={`${Style.box} ${Style.small}`}>작은 박스</div>;
+        return <BoxSmall>작은 박스</BoxSmall>;
     }
 }
--- Inflearn_React/create_react_app_v1.1/cra-test/src/Button.js
+++ Inflearn_React/create_react_app_v1.1/cra-test/src/Button.js
@@ -2,8 +2,10 @@ import React from 'react';
 // import './Button.css';
 import Style from './Button.module.css';
 import cn from 'classnames';
+import styled from 'styled-components';
 console.log({Style})
 
+/* 일반적인 CSS */
 // export default function Button({size}){
 //     if(size==='big'){
 //         return <button className="button big">큰 버튼</button>;
@@ -12,6 +14,8 @@ console.log({Style})
 //     }
 // }
 
+
+/* css-module */
 // export default function Button({size}){
 //     if(size=== 'big'){
 //         return <button className={`${Style.button} ${Style.big}`}>큰 버튼</button>;
@@ -23,12 +27,33 @@ console.log({Style})
 // }
 
 
-export default function Button({size}){
-    if(size=== 'big'){
-        return <button className={cn(Style.Button, Style.big)}>큰 버튼</button>;
-    }else{
-        return (
-            <button className={cn(Style.Button, Style.small)}>작은 버튼</button>
-        );
-    }
+/* classnames 사용 */
+// export default function Button({size}){
+//     if(size=== 'big'){
+//         return <button className={cn(Style.Button, Style.big)}>큰 버튼</button>;
+//     }else{
+//         return (
+//             <button className={cn(Style.Button, Style.small)}>작은 버튼</button>
+//         );
+//     }
+// }
+
+/* classnames 사용 */
+// export default function Button({size}){
+//     const isBig = size === 'big';
+//         return <button className={cn(Style.Button, {[Style.big]:isBig, [Style.small]: !isBig})}>{isBig? '큰 버튼': '작은 버튼'}</button>;
+// }
+
+
+/* css-in-js */
+//  아래 속성값을 받아서 동적으로 처리 가능
+const BoxCommon = styled.button`
+width: ${props => (props.isBig ? 100: 50)}px;
+height: 30px;
+background-color: red;`
+
+export default function Box({size}){
+    const isBig = size === 'big';
+    const label = isBig? '큰 버튼': '작은 버튼';
+    return <BoxCommon isBig={isBig}>{label}</BoxCommon>;
 }
