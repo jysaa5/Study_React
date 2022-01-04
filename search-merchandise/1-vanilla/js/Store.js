96f3a984e702cfebe434359a2f5fcdeb17a02d7a
jysaa5 <57422057+jysaa5@users.noreply.github.com>
2022-01-04 23:13:32 +0900

    Update project

--- search-merchandise/1-vanilla/js/Store.js
+++ search-merchandise/1-vanilla/js/Store.js
@@ -1,5 +1,6 @@
 const tag = "[store]";
 
+// MVC중 Model 역할
 export default class Store {
   constructor(storage) {
     if (!storage) throw "no storage";
---
+++ search-merchandise/1-vanilla/js/views/View.js
@@ -0,0 +1,37 @@
+import { emit, on } from "../helpers.js";
+
+const tag = "[View]";
+// MVC중 View
+export default class View {
+  // 생성자
+  constructor(element) {
+    if (!element) throw "no element";
+
+    this.element = element;
+    this.originalDisplay = this.element.style.dispaly || "";
+
+    return this;
+  }
+
+  hide() {
+    this.element.style.display = "none";
+    return this;
+  }
+
+  show() {
+    this.element.style.display = this.originalDisplay; // 보통 block이나 inline 값이 들어온다.
+    return this;
+  }
+
+  // 사용자와 인터렉션을 하면 DOM은 Event를 발생시킨다. 그 이벤트를 수신하는 것(구독하는 것)의 함수
+  on(eventName, handler) {
+    on(this.element, eventName, handler); // 유틸리티성 함수
+    return this;
+  }
+
+  // 이벤트 발행
+  emit(eventName, data) {
+    emit(this.element, eventName, data);
+    return this;
+  }
+}
