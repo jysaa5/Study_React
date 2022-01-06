98403d4ccf57c3b13efa7d5573fffb1fa399f366
jysaa5 <57422057+jysaa5@users.noreply.github.com>
2022-01-07 00:22:16 +0900

    Update SearchFormView.js

--- search-merchandise/1-vanilla/js/views/SearchFormView.js
+++ search-merchandise/1-vanilla/js/views/SearchFormView.js
@@ -1,14 +1,26 @@
-import { qs } from "../helpers.js";
+import { qs, on } from "../helpers.js";
 import View from "./View.js";
-
+const tag = "[SearchFormView]";
 export default class SearchFormView extends View {
   constructor() {
     super(qs("#search-form-view"));
+    this.inputElement = qs("[type=text]", this.element);
     this.resetElement = qs("[type=reset]", this.element);
     this.showResetButton(false);
+    this.bindEvent();
   }
 
   showResetButton(visible = ture) {
     this.resetElement.style.display = visible ? "block" : "none";
+  }
+
+  bindEvent() {
+    on(this.inputElement, "keyup", () => this.handleKeyup());
+  }
+
+  handleKeyup() {
+    console.log(tag, "handleKeyUp", this.inputElement.value);
+    const { value } = this.inputElement;
+    this.showResetButton(value.length > 0);
   }
 }
