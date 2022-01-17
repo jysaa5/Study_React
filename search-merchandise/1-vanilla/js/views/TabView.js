09ede40d0f4ff2f79ccdc6a15b7c672ce7a694ce
jysaa5 <57422057+jysaa5@users.noreply.github.com>
2022-01-17 23:17:35 +0900

    Add tab

---
+++ search-merchandise/1-vanilla/js/views/TabView.js
@@ -0,0 +1,43 @@
+import View from "./View.js";
+import { qs } from "../helpers.js";
+
+const TabType = {
+  KEYWORD: "KEYWORD",
+  HISTORY: "HISTORY",
+};
+
+const TabLabel = {
+  [TabType.KEYWORD]: "추천 검색어",
+  [TabType.HISTORY]: "최근 검색어",
+};
+
+export default class TabView extends View {
+  constructor() {
+    super(qs("#tab-view"));
+    this.template = new Template();
+  }
+
+  show() {
+    this.element.innerHTML = this.template.getTabList();
+    super.show();
+  }
+}
+
+class Template {
+  getTabList() {
+    return `
+        <ul>
+        ${Object.values(TabType)
+          .map((tabType) => ({ tabType, tabLabel: TabLabel[tabType] }))
+          .map(this._getTab)
+          .join("")}
+        </ul>
+        `;
+  }
+
+  _getTab({ tabType, tabLabel }) {
+    return `<li data-tab="${tabType}">
+        ${tabLabel}
+        </li>`;
+  }
+}
