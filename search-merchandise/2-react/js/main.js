9471c465f6d01e7ef6832e57c6d155ea8861c80d
jysaa5 <57422057+jysaa5@users.noreply.github.com>
2022-02-19 18:13:05 +0900

    Update main.js

--- search-merchandise/2-react/js/main.js
+++ search-merchandise/2-react/js/main.js
@@ -2,7 +2,20 @@
 
 class App extends React.Component {
   // render 함수 overriding -> react element를 반환해야 한다. component가 react element 대로 DOM을 그리도록 한다.
-  render() {}
+  render() {
+    return (
+      <>
+        <header>
+          <h2 className="container">검색</h2>
+        </header>
+        <div className="container">
+          <form>
+            <input type="text" placeholder="검색어를 입력해주세요." autoFocus></input>
+          </form>
+        </div>
+      </>
+    );
+  }
 }
 const element = (
   //<div>
@@ -18,4 +31,5 @@ const element = (
   </>
   //</div>
 ); // 자바스크립트 엔진이 자동으로 세미콜론을 붙이는 것을 방지하기 위해 소괄호로 감싼다. -> JSX를 사용할 때 권장하는 방식이다.
-ReactDOM.render(element, document.querySelector("#app"));
+//ReactDOM.render(element, document.querySelector("#app"));
+ReactDOM.render(<App />, document.querySelector("#app"));
