8855e5da2df043d947b8ac4404a3f7bc4849c8a7
JooYeon_Kim <jysaa5@naver.com>
2022-03-15 00:30:06 +0900

    Add Header component

---
+++ search-merchandise/3-component/src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Header from "./components/Header.js";
+
+export default class App extends React.Component {
+  render() {
+    return <Header />;
+  }
+}
--- search-merchandise/3-component/src/main.js
+++ search-merchandise/3-component/src/main.js
@@ -1,1 +1,5 @@
-console.log("main.js");
+import React from "react"; // JSX 문법을 사용하는 곳에서는 React를 가져와야한다.
+import ReactDOM from "react-dom";
+import App from "./App.js";
+
+ReactDOM.render(<App />, document.querySelector("#app"));
