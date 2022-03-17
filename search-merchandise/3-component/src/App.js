a23f2ba7ec2ce3a9f80c79d69c36c52519d16f1e
jysaa5 <jysaa5@naver.com>
2022-03-17 12:40:00 +0900

    Update reset and searchResult

--- search-merchandise/3-component/src/App.js
+++ search-merchandise/3-component/src/App.js
@@ -1,33 +1,44 @@
 import React from "react";
 import Header from "./components/Header.js";
 import SearchForm from "./components/SearchForm.js";
+import SearchResult from "./components/SearchResult.js";
+import store from "./Store.js";
 
 export default class App extends React.Component {
   constructor() {
     super();
-    this.state = { searchKeyowrd: "" };
+    this.state = {
+      searchKeyword: "",
+      searchResult: [],
+      submitted: false,
+    };
   }
   search(searchKeyword) {
     console.log("TODO: search >>>", searchKeyword);
+    const searchResult = store.search(searchKeyword);
+    this.setState({ searchResult, submitted: true });
   }
 
   handleReset() {
     console.log("TODO: handleReset");
+    this.setState({ searchKeyword: "", submitted: false, searchResul: [] });
   }
 
-  handleChangeInput(searchKeyowrd) {
-    if (searchKeyowrd.length <= 0) {
+  handleChangeInput(searchKeyword) {
+    if (searchKeyword.length <= 0) {
       this.handleReset();
     }
-    this.setState({ searchKeyowrd });
+    this.setState({ searchKeyword });
   }
 
   render() {
+    const { searchKeyword, submitted, searchResult } = this.state;
     return (
       <>
         <Header title="검색" />
         <div className="container">
-          <SearchForm value={this.state.searchKeyowrd} onChange={(value) => this.handleChangeInput(value)} onSubmit={(searchKeyword) => this.search(searchKeyword)} onReset={() => this.handleReset()} />
+          <SearchForm value={searchKeyword} onChange={(value) => this.handleChangeInput(value)} onSubmit={() => this.search(searchKeyword)} onReset={() => this.handleReset()} />
+          <div className="content">{submitted && <SearchResult data={searchResult} />}</div>
         </div>
       </>
     );
---
+++ search-merchandise/3-component/src/Store.js
@@ -0,0 +1,56 @@
+import storage from "./storage.js";
+import { createNextId } from "./helpers.js";
+const tag = "[Store]";
+
+// DB 역할
+class Store {
+  constructor(storage) {
+    console.log(tag, "constructor");
+    if (!storage) throw "no storage";
+
+    this.storage = storage;
+  }
+
+  search(keyword) {
+    this.addHistory(keyword);
+    return this.storage.productData.filter((product) => product.name.includes(keyword));
+  }
+
+  getKeywordList() {
+    return this.storage.keywordData;
+  }
+
+  getHistoryList() {
+    return this.storage.historyData.sort(this._sortHistory);
+  }
+
+  _sortHistory(history1, history2) {
+    return history2.date - history1.date;
+  }
+
+  removeHistory(keyword) {
+    this.storage.historyData = this.storage.historyData.filter((history) => history.keyword !== keyword);
+  }
+
+  addHistory(keyword) {
+    keyword = keyword.trim();
+    if (!keyword) {
+      return;
+    }
+
+    const hasHistory = this.storage.historyData.some((history) => history.keyword === keyword);
+    if (hasHistory) {
+      this.removeHistory(keyword);
+    }
+
+    const id = createNextId(this.storage.historyData);
+    const date = new Date();
+    console.log(date);
+    this.storage.historyData.push({ id, keyword, date });
+    this.storage.historyData = this.storage.historyData.sort(this._sortHistory);
+  }
+}
+
+// 싱글톤 패턴
+const store = new Store(storage);
+export default store;
