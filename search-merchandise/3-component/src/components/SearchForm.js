569ecda421ca724b51a9f5db9fac58d1a74fbb80
jysaa5 <jysaa5@naver.com>
2022-04-07 01:40:52 +0900

    Update onSubmit, onReset

--- search-merchandise/3-component/src/components/SearchForm.js
+++ search-merchandise/3-component/src/components/SearchForm.js
@@ -10,12 +10,26 @@ export default class SearchForm extends React.Component {
 
   handleChangeInput(event) {
     const searchKeyword = event.target.value;
+
+    if (searchKeyword.length <= 0) {
+      this.handleReset();
+    }
+
     this.setState({ searchKeyword });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.props.onSubmit(this.state.searchKeyword);
+  }
+
+  handleReset() {
+    this.props.onReset();
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={(event) => this.handleSubmit(event)} onReset={() => this.handleReset()}>
         <input type="text" placeholder="검색어를 입력해주세요." autoFocus value={this.state.searchKeyword} onChange={(event) => this.handleChangeInput(event)}></input>
         {this.state.searchKeyword.length > 0 && <button type="reset" className="btn-reset"></button>}
       </form>
