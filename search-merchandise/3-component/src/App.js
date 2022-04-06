563de6f12179b434e526849a3eb42c939298fae9
jysaa5 <jysaa5@naver.com>
2022-04-07 01:23:06 +0900

    Update app.js

--- search-merchandise/3-component/src/App.js
+++ search-merchandise/3-component/src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import Header from "./components/Header.js";
+import SearchForm from "./components/SearchForm.js";
 
 export default class App extends React.Component {
   render() {
     return (
       <>
         <Header title="검색" />
-        <Header title="프로필" />
-        <Header title="상품" />
+        <div className="container">
+          <SearchForm />
+        </div>
       </>
     );
   }
