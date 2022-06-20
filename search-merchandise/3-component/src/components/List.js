4591fb495d0121ff3d9ffe9f338e291b682254bc
jysaa5 <jysaa5@naver.com>
2022-06-20 22:14:19 +0900

    Update List

--- search-merchandise/3-component/src/components/List.js
+++ search-merchandise/3-component/src/components/List.js
@@ -1,10 +1,28 @@
 import React from "react";
-class List extends React.Component {
+export default class List extends React.Component {
   constructor() {
     super();
 
     this.state = {
       data: [],
     };
+  }
+
+  renderItem(item, index) {
+    throw "renderItem()을 구현하세요";
+  }
+
+  render() {
+    return (
+      <ul className="list">
+        {this.state.data.map((item, index) => {
+          return (
+            <li key={item.id} onClick={() => this.props.onClick(item.keyword)}>
+              {this.renderItem(item, index)}
+            </li>
+          );
+        })}
+      </ul>
+    );
   }
 }
