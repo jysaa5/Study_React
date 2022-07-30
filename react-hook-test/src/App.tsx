22253eda983ae345751a4bd8fcfab94d8a34e68f
jysaa5 <jysaa5@naver.com>
2022-07-31 01:55:18 +0900

    useRef로 DOM에 접근

--- react-hook-test/src/App.tsx
+++ react-hook-test/src/App.tsx
@@ -36,17 +36,31 @@ const App = () => {
     console.log("랜더링 수:", renderCount.current);
   });
 
+  const inputRef = useRef<HTMLInputElement>(null);
+  // 맨 처음 랜더링 될 때만 동작
+  useEffect(() => {
+    console.log(inputRef);
+    alert(`환영합니다 ${inputRef.current?.value}!`);
+    inputRef.current?.focus();
+  }, []);
+
   return (
-    <div>
-      <p>State: {count}</p>
-      <p>Ref: {countRef.current}</p>
-      <button onClick={increaseCountState}>Add State</button>
-      <button onClick={increaseCountRef}>Add Ref</button>
-      <p>Var: {countVar}</p>
-      <button onClick={increaseCountVar}>Add Var</button>
-      <button onClick={doRendering}>Render</button>
-      <button onClick={printResults}>Print Ref, Var</button>
-    </div>
+    <>
+      <div>
+        <p>State: {count}</p>
+        <p>Ref: {countRef.current}</p>
+        <button onClick={increaseCountState}>Add State</button>
+        <button onClick={increaseCountRef}>Add Ref</button>
+        <p>Var: {countVar}</p>
+        <button onClick={increaseCountVar}>Add Var</button>
+        <button onClick={doRendering}>Render</button>
+        <button onClick={printResults}>Print Ref, Var</button>
+      </div>
+      <div>
+        <input type="text" placeholder="username" ref={inputRef}></input>
+        <button>로그인</button>
+      </div>
+    </>
   );
 };
 
