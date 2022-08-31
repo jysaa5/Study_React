14aca3e3143c228afb59e57f336f47193c9f3047
JooYeon_Kim <jysaa5@naver.com>
2022-08-31 11:08:35 +0900

    최근 검색어
    - 최근 검색어 시간 추가
    - 삭제 버튼 오류 수정

--- search-merchandise/3-component/src/components/HistoryList.js
+++ search-merchandise/3-component/src/components/HistoryList.js
@@ -1,17 +1,30 @@
 import React from "react";
-import List from "./List.js";
+import { formatRelativeDate } from "../helpers.js";
 import store from "../Store.js";
+import { List } from "./List.js";
 
 export default class HistoryList extends List {
   componentDidMount() {
+    this.fetch();
+  }
+
+  fetch() {
     const data = store.getHistoryList();
     this.setState({ data });
   }
-  renderItem(item, index) {
+
+  handleClickRemoveHistory(event, keyword) {
+    event.stopPropagation();
+    store.removeHistory(keyword);
+    this.fetch();
+  }
+
+  renderItem(item) {
     return (
       <>
-        <span className="number">{index + 1}</span>
         <span>{item.keyword}</span>
+        <span className="date">{formatRelativeDate(item.data)}</span>
+        <button className="btn-remove" onClick={(event) => this.handleClickRemoveHistory(event, item.keyword)}></button>
       </>
     );
   }
