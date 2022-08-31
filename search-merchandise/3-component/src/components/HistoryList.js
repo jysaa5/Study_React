52b665864173953756b16f8b5abc56a3eee93ad5
jysaa5 <jysaa5@naver.com>
2022-08-31 21:55:15 +0900

    Revert "최근 검색어 목록"
    This reverts commit 534d5d3de8e4efb94bc8d3becf7fce7bec46a7f6.

--- search-merchandise/3-component/src/components/HistoryList.js
+++ search-merchandise/3-component/src/components/HistoryList.js
@@ -1,29 +1,17 @@
 import React from "react";
 import List from "./List.js";
 import store from "../Store.js";
-import { formatRelativeDate } from "../helpers.js";
 
 export default class HistoryList extends List {
   componentDidMount() {
-    this.fetch();
-  }
-
-  fetch() {
     const data = store.getHistoryList();
     this.setState({ data });
   }
-
-  handleClickRemoveHistory(event, keyword) {
-    event.stopPropagation();
-    store.removeHistory(keyword);
-    this.fetch();
-  }
-  renderItem(item) {
+  renderItem(item, index) {
     return (
       <>
+        <span className="number">{index + 1}</span>
         <span>{item.keyword}</span>
-        <span className="date">{formatRelativeDate(item.date)}</span>
-        <button className="btn-remove" onClick={(event) => this.handleClickRemoveHistory(event, item.keyword)}></button>
       </>
     );
   }
