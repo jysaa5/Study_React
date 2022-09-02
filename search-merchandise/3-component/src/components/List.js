57f5fdf292431ff2aff1ec2544735d4a21b8e80e
JooYeon_Kim <jysaa5@naver.com>
2022-09-02 11:24:07 +0900

    상속으로 변경
    컴포넌트 상속으로 변경

--- search-merchandise/3-component/src/components/List.js
+++ search-merchandise/3-component/src/components/List.js
@@ -1,15 +1,31 @@
 import React from "react";
-const List = ({ data = [], onClick, renderItem }) => {
-  return (
-    <ul className="list">
-      {data.map((item, index) => {
-        return (
-          <li key={item.id} onClick={() => onClick(item.keyword)}>
-            {renderItem(item, index)}
-          </li>
-        );
-      })}
-    </ul>
-  );
-};
-export default List;
+export class List extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      data: [],
+    };
+  }
+
+  renderItem(item, index) {
+    throw "renderItem()을 구현하세요";
+  }
+
+  render() {
+    const { onClick } = this.props;
+    const { data } = this.state;
+
+    return (
+      <ul className="list">
+        {data.map((item, index) => {
+          return (
+            <li key={item.id} onClick={() => onClick(item.keyword)}>
+              {this.renderItem(item, index)}
+            </li>
+          );
+        })}
+      </ul>
+    );
+  }
+}
