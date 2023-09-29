844cf479c0e2c6de1234d5bde5490f7c541d5eaf
jysaa5 <jysaa5@naver.com>
2023-09-29 22:18:27 +0900

    :lipstick: todo list 레이아웃으로 수정

--- react-app-1/src/App.tsx
+++ react-app-1/src/App.tsx
@@ -1,24 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>My Todos</h1>
+      <div className="card">
+        <h2>TITLE</h2>
+        <div className="actions">
+          <button className="btn">Delete</button>
+        </div>
+      </div>
     </div>
   );
 }
