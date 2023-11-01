9eb7ab4b3bb3ef1aea5428b5a969ec454fa2d48e
jysaa5 <jysaa5@naver.com>
2023-11-02 00:35:54 +0900

    react router 추거

--- react-app-1/src/App.tsx
+++ react-app-1/src/App.tsx
@@ -1,12 +1,7 @@
 import Todo from "./components/Todo";
 
 function App() {
-  return (
-    <div>
-      <h1>My Todos</h1>
-      <Todo text="할 일" />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
