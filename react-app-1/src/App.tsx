4d1aa0fabb2dce7a558bce541896749631dfd8a8
jysaa5 <jysaa5@naver.com>
2024-01-01 19:59:46 +0900

    :lipstick: 네비게이션 바 추가

--- react-app-1/src/App.tsx
+++ react-app-1/src/App.tsx
@@ -2,10 +2,12 @@ import { Route, Routes } from "react-router-dom";
 import AllMeetups from "./pages/AllMeetups";
 import NewMeetupPage from "./pages/NewMeetup";
 import FavoritesPage from "./pages/Favorites";
+import MainNavigation from "./components/layout/MainNavigation";
 
 function App() {
   return (
     <div>
+      <MainNavigation />
       <Routes>
         <Route path="/" element={<AllMeetups />} />
         <Route path="/new-meetup" element={<NewMeetupPage />} />
