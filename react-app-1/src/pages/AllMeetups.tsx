f1c84ab073669cd7e9f145749a724bad6c4d68d2
jysaa5 <jysaa5@naver.com>
2024-01-03 02:53:24 +0900

    더미데이터 변경

--- react-app-1/src/pages/AllMeetups.tsx
+++ react-app-1/src/pages/AllMeetups.tsx
@@ -1,8 +1,28 @@
 // 모든 모임 약속을 로드하고 표시하는 컴포넌트
 
-const dumpyData = [];
+const DUMMY_DATA = [
+  {
+    id: "m1",
+    title: "This is a first meetup",
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
+    address: "Meetupstreet 5, 12345 Meetup City",
+    description: "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
+  },
+  {
+    id: "m2",
+    title: "This is a second meetup",
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
+    address: "Meetupstreet 5, 12345 Meetup City",
+    description: "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
+  },
+];
+
 function AllMeetupsPage() {
-  return <div>All Meetups Page</div>;
+  return (
+    <section>
+      <h1>All Meetups Page</h1>
+    </section>
+  );
 }
 
 export default AllMeetupsPage;
