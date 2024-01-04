d70901e7013a5640bc1627270c0319ae00c1f6d6
jysaa5 <jysaa5@naver.com>
2024-01-04 23:57:06 +0900

    :triangular_flag_on_post: 모임 리스트 조회 컴포넌트 개발

---
+++ react-app-1/src/components/meetups/MeetupItem.tsx
@@ -0,0 +1,19 @@
+function MeetupItem() {
+  return (
+    <li>
+      <div>
+        <img src="" alt="" />
+      </div>
+      <div>
+        <h3>TITLE</h3>
+        <address>ADDRESS</address>
+        <p>DESCRIPTION</p>
+      </div>
+      <div>
+        <button>To Favorites</button>
+      </div>
+    </li>
+  );
+}
+
+export default MeetupItem;
