6368f106e1b4597f638cd30fbbf99f48ee1f3d74
jysaa5 <jysaa5@naver.com>
2024-01-09 03:57:00 +0900

    새로운 모임 추가 form 컴포넌트 개발중

---
+++ react-app-1/src/components/meetups/NewMeetupForm.tsx
@@ -0,0 +1,10 @@
+import Card from "../ui/Card";
+import classes from "./NewMeetupForm.module.css";
+function NewMeetupForm() {
+  return (
+    <Card>
+      <form className={classes.form}></form>
+    </Card>
+  );
+}
+export default NewMeetupForm;
--- react-app-1/src/pages/NewMeetup.tsx
+++ react-app-1/src/pages/NewMeetup.tsx
@@ -1,9 +1,12 @@
 // 새 모임 약속을 추가
 
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+
 function NewMeetupPage() {
   return (
     <section>
-      <h1></h1>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm />
     </section>
   );
 }
