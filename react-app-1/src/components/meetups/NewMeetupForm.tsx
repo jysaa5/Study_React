a8a98ec3959fe5c867f9a2296b05f9d78c8cc2da
jysaa5 <jysaa5@naver.com>
2024-01-09 04:02:58 +0900

    form 레이아웃 개발

--- react-app-1/src/components/meetups/NewMeetupForm.tsx
+++ react-app-1/src/components/meetups/NewMeetupForm.tsx
@@ -3,7 +3,27 @@ import classes from "./NewMeetupForm.module.css";
 function NewMeetupForm() {
   return (
     <Card>
-      <form className={classes.form}></form>
+      <form className={classes.form}>
+        <div className={classes.control}>
+          <label htmlFor="title">Meetup Title</label>
+          <input type="text" required id="title" />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="image">Meetup Image</label>
+          <input type="url" required id="image" />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="address">Meetup Address</label>
+          <input type="text" required id="address" />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="description">Description</label>
+          <textarea rows={5} required id="description" />
+        </div>
+        <div className={classes.actions}>
+          <button>Add Meetup</button>
+        </div>
+      </form>
     </Card>
   );
 }
