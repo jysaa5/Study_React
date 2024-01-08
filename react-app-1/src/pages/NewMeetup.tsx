012bf8ef7db5d75dee5a2ed264657364c7734927
jysaa5 <jysaa5@naver.com>
2024-01-08 23:57:34 +0900

    새로운 모임 추가하는 form 컴포넌트 개발중

--- react-app-1/src/pages/NewMeetup.tsx
+++ react-app-1/src/pages/NewMeetup.tsx
@@ -1,7 +1,11 @@
 // 새 모임 약속을 추가
 
 function NewMeetupPage() {
-  return <div>New Meetup Page</div>;
+  return (
+    <section>
+      <h1></h1>
+    </section>
+  );
 }
 
 export default NewMeetupPage;
