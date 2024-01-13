a6f8673dfed13215ea3c49d8a1435f486d79211b
jysaa5 <jysaa5@naver.com>
2024-01-14 03:17:14 +0900

    네비게이션 기능 추가중

--- react-app-1/src/pages/NewMeetup.tsx
+++ react-app-1/src/pages/NewMeetup.tsx
@@ -1,10 +1,13 @@
 // 새 모임 약속을 추가
-import { useHistory } from "react-router";
+import { useHistory } from "react-router-dom";
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
+  const history = useHistory();
   function addMeetupHandler(meetupData: { title: string; image: string; address: string; description: string }) {
-    fetch("https://react-getting-started-c5dcd-default-rtdb.firebaseio.com/meetups.json", { method: "post", body: JSON.stringify(meetupData), headers: { "Content-Type": "application/json" } });
+    fetch("https://react-getting-started-c5dcd-default-rtdb.firebaseio.com/meetups.json", { method: "post", body: JSON.stringify(meetupData), headers: { "Content-Type": "application/json" } }).then(() => {
+      history.replace("/");
+    });
   }
 
   return (
