896663d8ff361075432970b16e89b825255a3b61
jysaa5 <jysaa5@naver.com>
2024-01-15 23:41:26 +0900

    react router dom 버전 6 설치 및 수정

--- react-app-1/src/pages/NewMeetup.tsx
+++ react-app-1/src/pages/NewMeetup.tsx
@@ -1,12 +1,12 @@
 // 새 모임 약속을 추가
-import { useHistory } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
-  const history = useHistory();
+  const navigate = useNavigate();
   function addMeetupHandler(meetupData: { title: string; image: string; address: string; description: string }) {
     fetch("https://react-getting-started-c5dcd-default-rtdb.firebaseio.com/meetups.json", { method: "post", body: JSON.stringify(meetupData), headers: { "Content-Type": "application/json" } }).then(() => {
-      history.replace("/");
+      navigate("/", { replace: true });
     });
   }
 
