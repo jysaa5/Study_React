f409097ab746516e30fe6b35d72f0deccda73915
jysaa5 <jysaa5@naver.com>
2024-01-20 21:06:49 +0900

    :triangular_flag_on_post: 즐겨찾기 개수 표시 기능 추가

--- react-app-1/src/pages/Favorites.tsx
+++ react-app-1/src/pages/Favorites.tsx
@@ -1,7 +1,25 @@
 // 가장 좋아하는 모임 약속을 모아서 봄
 
+import { useContext } from "react";
+import FavoritesContext from "../store/favorites-context";
+import MeetupList from "../components/meetups/MeetupList";
+
 function FavoritesPage() {
-  return <div>Favorites Page</div>;
+  const favoritesCtx = useContext(FavoritesContext);
+  let content = {} as React.ReactNode;
+
+  if (favoritesCtx.totalFavorites === 0) {
+    content = <p>You got no favorite yet. Start adding some?</p>;
+  } else {
+    content = <MeetupList meetups={favoritesCtx.favorites} />;
+  }
+
+  return (
+    <section>
+      <h1>My Favorites</h1>
+      {content}
+    </section>
+  );
 }
 
 export default FavoritesPage;
