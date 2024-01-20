245045dd41221096161406d9cd4eb6f2708e04f4
jysaa5 <jysaa5@naver.com>
2024-01-20 20:47:55 +0900

    :triangular_flag_on_post: meetup 즐겨찾기 추가 기능

--- react-app-1/src/components/meetups/MeetupItem.tsx
+++ react-app-1/src/components/meetups/MeetupItem.tsx
@@ -1,7 +1,20 @@
 import classes from "./MeetupItem.module.css";
 import Card from "../ui/Card";
+import { useContext } from "react";
+import FavoritesContext from "../../store/favorites-context";
 
 function MeetupItem(props: { id: string; title: string; image: string; address: string; description: string }) {
+  const favoritCtx = useContext(FavoritesContext);
+
+  const itemIsFavorite = favoritCtx.itemIsFavorite(props.id);
+
+  function toggleFavoriteStatusHandler() {
+    if (Boolean(itemIsFavorite)) {
+      favoritCtx.removeFavorite(props.id);
+    } else {
+      favoritCtx.addFavorite({ id: props.id, title: props.title, image: props.image, address: props.address, description: props.description });
+    }
+  }
   return (
     <li className={classes.item}>
       <Card>
@@ -14,7 +27,7 @@ function MeetupItem(props: { id: string; title: string; image: string; address: 
           <p>{props.description}</p>
         </div>
         <div className={classes.actions}>
-          <button>To Favorites</button>
+          <button onClick={toggleFavoriteStatusHandler}>{itemIsFavorite ? "Remove from favorites" : "To Favorites"}</button>
         </div>
       </Card>
     </li>
--- react-app-1/src/store/favorites-context.tsx
+++ react-app-1/src/store/favorites-context.tsx
@@ -4,24 +4,44 @@ import { Data } from "../types";
 interface Props {
   children: React.ReactNode;
 }
-const FavoritesContext = createContext({ favorites: [] as Data[], totalFavorites: 0 });
 
-function FavoritesContextProvider(props: Props) {
+export interface Favorites {
+  favorites: Data[];
+  addFavorite: (favoriteMeetup: Data) => void;
+  itemIsFavorite: (meetupId: string) => boolean;
+  removeFavorite: (meetupId: string) => void;
+  totalFavorites: number;
+}
+
+const FavoritesContext = createContext({} as Favorites);
+
+export function FavoritesContextProvider(props: Props) {
   const [userFavorites, setUserFavorites] = useState<Data[]>([]);
 
   function addFavoriteHandler(favoriteMeetup: Data) {
-    setUserFavorites((prevFavorites) => {
-      return prevFavorites.concat(favoriteMeetup);
+    setUserFavorites((prevUserFavorites) => {
+      return prevUserFavorites.concat(favoriteMeetup);
     });
   }
 
-  function removeFavoriteHandler() {}
+  function removeFavoriteHandler(meetupId: string) {
+    setUserFavorites((prevUserFavorites) => {
+      return prevUserFavorites.filter((meetup) => meetup.id !== meetupId);
+    });
+  }
 
-  function itemIsFavoriteHandler() {}
+  function itemIsFavoriteHandler(meetupId: string) {
+    return userFavorites.some((meetup) => meetup.id === meetupId);
+  }
 
   const context = {
     favorites: userFavorites,
     totalFavorites: userFavorites.length,
+    addFavorite: addFavoriteHandler,
+    removeFavorite: removeFavoriteHandler,
+    itemIsFavorite: itemIsFavoriteHandler,
   };
   return <FavoritesContext.Provider value={context}>{props.children} </FavoritesContext.Provider>;
 }
+
+export default FavoritesContext;
